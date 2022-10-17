56e7fed4e73932db8c8be26fdfc21d9c6d6c0cc4
QuixoticKel <52431022+QuixoticKel@users.noreply.github.com>
2022-10-16 19:43:59 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ def get_fruityvice_data(this_fruit_choice):
     return fruityvice_normalized
  
 #section to display response
-streamlit.header('Fruit List')
+streamlit.header('FruityVice Fruit List')
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
